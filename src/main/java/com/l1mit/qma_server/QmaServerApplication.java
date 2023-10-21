781b887346a679b6ef92578ec3de0648046d6d7b
lim1t <namest504@gmail.com>
2023-10-21 16:38:03 +0900

    Initial commit

---
+++ src/main/java/com/l1mit/qma_server/QmaServerApplication.java
@@ -0,0 +1,13 @@
+package com.l1mit.qma_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QmaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/l1mit/qma_server/QmaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l1mit.qma_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
