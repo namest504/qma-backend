551d2f836f3d7723e60862b9e73aac9840669784
lim1t <namest504@gmail.com>
2023-10-22 10:45:18 +0900

    test: #1 서버 시간대 설정 테스트

--- src/test/java/com/l1mit/qma_server/QmaServerApplicationTests.java
+++ src/test/java/com/l1mit/qma_server/QmaServerApplicationTests.java
@@ -1,13 +1,20 @@
 package com.l1mit.qma_server;
 
+import java.util.TimeZone;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class QmaServerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@DisplayName("서버의 시간대는 서울이다.")
+	public void TimeZoneSetting() {
+		QmaServerApplication app = new QmaServerApplication();
+		app.started();
+
+		Assertions.assertThat(TimeZone.getDefault())
+				.isEqualTo(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
