6ee9a8771c356d8b7d5eb58d7d4142ed9539f83b
lim1t <namest504@gmail.com>
2023-11-05 17:53:35 +0900

    fix: #10 Filter 에러 처리방식 수정

--- src/main/java/com/l1mit/qma_server/global/filter/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/l1mit/qma_server/global/filter/JwtAuthenticationEntryPoint.java
@@ -1,9 +1,8 @@
 package com.l1mit.qma_server.global.filter;
 
-import com.l1mit.qma_server.global.exception.ErrorCode;
-import com.l1mit.qma_server.global.exception.QmaApiException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -13,7 +12,11 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
-            AuthenticationException authException) {
+            AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증 에러가 발생하였습니다.");
+        /*
+        todo: ControllerAdvice 에서 처리할 수 있도록 로직 추가 필요
         throw new QmaApiException(ErrorCode.UNAUTHORIZED);
+        */
     }
 }
